850ca85b8e68f47112f6f224f21d4d8ae11b9930
Marcin <Majsyn@gmail.com>
2018-07-23 13:11:24 +0200

    drobne poprawki zadania 11.

--- src/Zadanie11.java
+++ src/Zadanie11.java
@@ -17,11 +17,11 @@ public class Zadanie11 {
             System.out.println("Punkt(" + xPoint + ", " + yPoint + ") leży w I ćwiartce układu współrzędnych");
         } else if (xPoint < 0 && yPoint < 0) {
             System.out.println("Punkt(" + xPoint + ", " + yPoint + ") leży w III ćwiartce układu współrzędnych");
-        } else if(xPoint == 0 && yPoint > 0 || yPoint < 0){
-            System.out.println("Punkt leży na osi Y");
-        }else if (yPoint == 0 && xPoint > 0 || xPoint < 0){
-            System.out.println("Punkt leży na osi X");
-        }else
+        } else if (xPoint == 0 && yPoint > 0 || yPoint < 0) {
+            System.out.println("Punkt(" + xPoint + ", " + yPoint + ") leży na osi Y w punkcie Y = " + yPoint);
+        } else if (yPoint == 0 && xPoint > 0 || xPoint < 0) {
+            System.out.println("Punkt(" + xPoint + ", " + yPoint + ") leży na osi X w punkcie X = " + xPoint);
+        } else
             System.out.println("Punkt(" + xPoint + ", " + yPoint + ") leży w punkcie 0 układu");
 
         input.close();
